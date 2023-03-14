99974deca3bb3050ad0f64bfa349f0df5b22542e
hschickdevs <64290193+hschickdevs@users.noreply.github.com>
2023-03-14 03:09:55 -0500

    complete sections 1-2

---
+++ content/personal-site/README.md
@@ -0,0 +1,3 @@
+# Personal Site
+
+I created a basic personal website to practice basic unstyled HTML
--- gen-dir.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-if __name__ == "__main__":
-    for i in range(41):
-        path = Path(f"section-{i + 1}")
-        if not path.exists():
-            path.mkdir()
